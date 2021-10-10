cb6ca4e1ed8c9479754b0be7a585bab80d172c55
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2021-10-10 23:24:54 +0900

    Add files via upload

---
+++ src/lecture03/input2.py
@@ -0,0 +1,2 @@
+num = input("数値データ入力: ")
+print(num + 10)
---
+++ src/lecture03/var_type.py
@@ -0,0 +1,4 @@
+variable = "いろは"
+print("変数variableの内容は" + variable + "です") 
+variable = 168
+print("変数variableの内容は" + str(variable) + "です") 
