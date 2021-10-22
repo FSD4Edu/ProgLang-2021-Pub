3854f30d522fe7db757a204bbf2f7b504383b9b8
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2021-10-22 10:02:43 +0900

    Create month_picker.py

---
+++ src/lecture04/month_picker.py
@@ -0,0 +1,3 @@
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+month = input("何月を調べますか？：")
+print(month + "月に対応する英単語は" +  + "です")
