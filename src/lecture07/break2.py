e1b7246835b300b4eb2dc825f297cfdedfb62a83
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2021-11-11 20:04:35 +0900

    Create break2.py

---
+++ src/lecture07/break2.py
@@ -0,0 +1,8 @@
+passwd = "himitsu"
+while True:
+    word = input("Please input the password : ")
+    if word == passwd :
+        print("CORRECT")
+        break
+    else:
+        print("INCORRECT")
