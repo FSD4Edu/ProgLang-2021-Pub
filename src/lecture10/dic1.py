ee4db16ebb6d9122e0de62da29e3ad8029771c24
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2021-12-03 10:32:58 +0900

    Create dic1.py

---
+++ src/lecture10/dic1.py
@@ -0,0 +1,7 @@
+dic = {"日":"Sun", "月":"Mon", "火":"Tue", "水":"Wed", "木":"Thu", "金":"Fri", "土":"Sat"}
+key = input("キーを入力してください：")
+if key in dic:
+    del dic[key]
+    print(dic)
+else:
+    print("キー『" + key + "』が見つかりませんでした")
