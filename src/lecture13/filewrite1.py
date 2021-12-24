3c91bf6b6f57b8666e961a2740279c4491a318de
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2021-12-24 10:20:30 +0900

    Create filewrite1.py

---
+++ src/lecture13/filewrite1.py
@@ -0,0 +1,6 @@
+with open("out.txt", "w", encoding = "utf_8") as file:
+    file.write("明日の天気です∖n")
+    file.write("関西地方はおおむね晴れ．")
+    file.write("紅葉を見るには絶好の日和でしょう∖n")
+    file.write(“映像は嵐山の様子です．")
+    file.write("今年も大変な数の観光客が訪れているようですね．∖n")
