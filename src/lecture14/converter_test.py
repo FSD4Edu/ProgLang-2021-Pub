39493d92716ecead85b57449192a50d432d5c525
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2022-01-07 09:30:49 +0900

    Create converter_test.py

---
+++ src/lecture14/converter_test.py
@@ -0,0 +1,8 @@
+import converter
+
+length = converter.syaku_to_cm(10)
+print("10尺=", length, "cm")
+weight = converter.kan_to_kg(10)
+print("10貫=", weight, "kg")
+volume = converter.sho_to_ml(10)
+print("10升=", volume  , "ml")
