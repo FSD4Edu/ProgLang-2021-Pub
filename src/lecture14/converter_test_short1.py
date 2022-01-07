1fbf80b8decbd04363b57767881fb87362fb3773
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2022-01-07 09:31:42 +0900

    Create converter_test_short1.py

---
+++ src/lecture14/converter_test_short1.py
@@ -0,0 +1,11 @@
+import converter
+
+s2cm = converter.syaku_to_cm
+length = s2cm(10)
+print("10尺=", length, "cm")
+k2kg = converter.kan_to_kg
+weight = k2kg(10)
+print("10貫=", weight, "kg")
+s2ml = converter.sho_to_ml
+volume = s2ml(10)
+print("10升=", volume  , "ml")
