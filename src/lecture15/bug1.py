11cba9eb7f22c3f8529e2e19eff63f5b9b0c2ffc
Hiromitsu HATTORI <hatto@fc.ritsumei.ac.jp>
2022-01-14 10:01:02 +0900

    Update bug1.py

--- src/lecture15/bug1.py
+++ src/lecture15/bug1.py
@@ -3,5 +3,5 @@ x1 = 11
 y1 = 5
 x2 = 4
 y2 = 3
-area = math.sqrt((x2-x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) / 2 * math.sqrt((x2-x1) * (x2 - x1) + (y2 - y1) * (y2 - x1)) /  3.14
+area = math.sqrt((x2-x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) / 2 * math.sqrt((x2-x1) * (x2 - x1) + (y2 - y1) * (y2 - x1)) / 2 * 3.14
 print(area)
